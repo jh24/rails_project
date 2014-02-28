27ca2b18cdafe71b3caca0c7dd69a542cebed9c7
Jason Heaton <jh24@live.missouristate.edu>
2014-02-28 02:54:28 -0600

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,11 +6,11 @@ require 'spec_helper'
 
 	it "should have the h1 'Sample App'" do
 	  visit '/static_pages/home'
-	  page.should have_seletor('h1', :text => 'Sample App')
+	  page.should have_selector('h1', :text => 'Sample App')
 	end
 
 	it "should have the base title" do
-	  visit 'static_pages/home'
+	  visit '/static_pages/home'
 	  page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
 	end
 
@@ -24,11 +24,11 @@ require 'spec_helper'
 
 	it "should have the h1 'Sample App'" do
 	  visit '/static_pages/help'
-	  page.should have_seletor('h1', :text => 'Sample App')
+	  page.should have_selector('h1', :text => 'Sample App')
 	end
 
 	it "should have the base title" do
-	  visit 'static_pages/help'
+	  visit '/static_pages/help'
 	  page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
 	end
 
@@ -43,11 +43,11 @@ require 'spec_helper'
 
 	it "should have the h1 'Sample App'" do
 	  visit '/static_pages/about'
-	  page.should have_seletor('h1', :text => 'Sample App')
+	  page.should have_selector('h1', :text => 'Sample App')
 	end
 
 	it "should have the base title" do
-	  visit 'static_pages/about'
+	  visit '/static_pages/about'
 	  page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
 	end
 
