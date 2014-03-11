63c5437ac2b57e47199c82374dae26c548b17c0b
Jason Heaton <jh24@live.missouristate.edu>
2014-03-11 16:56:30 -0500

    Ch 7.1.3

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,10 @@ RailsApp::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # Speed up tests by lowering BCrypt's cost funcion
+  require 'bcrypt'
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engint::MIN_COST
+  end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name	"Jason Heaton"
+    email	"jh24@live.missouristate.edu"
+    password	"foobar"
+    password_confirmation  "foobar"
+  end
+end
