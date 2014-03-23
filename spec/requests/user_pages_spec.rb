6ac89fb8a22208c6edabc249855245d46e553e06
Jason Heaton <jh24@live.missouristate.edu>
2014-03-23 02:41:53 -0500

    Finish pg 475

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -55,8 +55,8 @@ describe "User Pages" do
 
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
-    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Hello" }
-    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "World!" }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Hello") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "World!") }
 
     before { visit user_path(user) }
 
@@ -85,7 +85,7 @@ describe "User Pages" do
 	  fill_in "Name",		with: "Example User"
 	  fill_in "Email",		with: "user@example.com"
 	  fill_in "Password",		with: "foobar"
-	  fill_in "Password confirmation", 	with: "foobar"  
+	  fill_in "Confirm password", 	with: "foobar"  
 	end
 
       it "should create a user" do
@@ -130,7 +130,7 @@ describe "User Pages" do
 	fill_in "Name", with: new_name
 	fill_in "Email", with: new_email
 	fill_in "Password", with: user.password
-	fill_in "Confirm Password", with: user.password
+	fill_in "Confirm password", with: user.password
 	click_button "Save changes"
       end
 
@@ -141,5 +141,6 @@ describe "User Pages" do
       specify { user.reload.email.should == new_email }
     end
   end 
+end
 end
 end
