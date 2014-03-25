8eceba7a1af5db93a6239d737f4daf1ac07f51fc
Jason Heaton <jh24@live.missouristate.edu>
2014-03-25 00:04:40 -0500

    Finish Ch 11.1

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -20,8 +20,18 @@ describe Relationship do
 
   describe "follower methods" do
     it { should respond_to(:follower) }
-    it { should respond_do(:followed) }
+    it { should respond_to(:followed) }
     its(:follower) { should == follower }
     its(:followed) { should == followed }
+  end
+
+  describe "when followed id is not present" do
+    before { relationship.followed_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when follower id is not present" do
+    before { relationship.follower_id = nil }
+    it { should_not be_valid }
   end
 end
